355fb50662b90950ef507d14847f1deabc1c313e
Raul da Mata Esteves <raul.m.esteves@hotmail.com>
2020-08-27 14:33:40 -0300

    Ajusta nome da pripriedade Descricao

--- ListaTarefas/Models/Tarefas.cs
+++ ListaTarefas/Models/Tarefas.cs
@@ -9,7 +9,7 @@ namespace ListaTarefas.Models
     {
         public int TarefasId { get; set; }
         public string Nome { get; set; }
-        public string descricao { get; set; }
+        public string Descricao { get; set; }
         public DateTime Inicio { get; set; }
         public DateTime Fim { get; set; }
         public string Importancia { get; set; }
